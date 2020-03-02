2c2d27fba50ac63d8fefd039907896e4bd6e5b57
Sophie168-mel <60349664+Sophie168-mel@users.noreply.github.com>
2020-03-02 15:07:09 +0100

    Add files via upload

---
+++ new.php
@@ -0,0 +1,43 @@
+<html>
+
+<body>
+	<h1> Inscription </h1>
+<form method="get" action="inscription.php" autocomplete="off"> 
+	<title> Inscription </title>
+<p>
+	Nom de famille <INPUT type="text" name="n" value="">
+</p>
+<p>
+	Prenom <INPUT type="text" name="p" value="">
+</p>
+<p>
+	Date de naissance <INPUT type="date" name="anniversaire" value="">
+</p>
+<p>
+	Adresse <INPUT type="text" name="adr" value="">
+</p>
+<p>
+	Code postal <INPUT type="number" name="cp" value="">
+</p>
+<p>
+	Ville <INPUT type="text" name="ville" value="">
+</p>
+<p>
+	Numéro de téléphone <INPUT type="number" name="num" value="">
+</p>
+<p>
+	Adresse électronique <INPUT type="text" name="mail" value="">
+</p>
+<p>
+	Mot de passe <INPUT type="password" name="mdp1" value="">
+</p>
+<p>
+	Confirmer votre mot de passe <INPUT type="password" name="mdp2" value="">
+</p>
+ <p>
+ 	 <INPUT type="submit" value="S'inscrire">
+ </p>
+</form>
+
+</body>
+</html>
