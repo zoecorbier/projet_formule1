f71e787f1b89b5e7ae29c7ee7d893511e17664b4
zoe.corbier <47537047+zoecorbier@users.noreply.github.com>
2020-03-16 15:37:34 +0100

    Add files via upload

---
+++ circuit.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="table.css" type="text/css" media="screen" />
+        <title>Pilote</title>
+        
+    </head>
+    
+    <nav class="projet">
+            <div class="projet">
+            <ul>
+                <li><a href="pageacceuil.html" style="text-decoration:none">Accueil</a></li>
+                <li><a href="constructeur.php" style="text-decoration:none">Données sur les constructeurs</a></li>
+            </ul>
+            </div>
+    </nav>
+    
+    
+    <body>
+        <table>
+            <h1>Données sur les circuits</h1>
+             <tr>
+			     <th>Nom</th>
+			     <th>Ville</th>
+			     <th>Pays du Circuit</th>
+		      </tr>
+            <tr>
+                <?php
+                 $bdd = new PDO('mysql:host=localhost;dbname=Projet_Formule1;charset=utf8', 'root', 'root');
+                $rep = $bdd->query('select * from Circuit');
+                
+                $ligne = $rep->fetch();
+                while ($ligne = $rep ->fetch()) { 
+		          echo "<tr>";
+		          //echo"<th>"<a href="article/".$ligne[nom].".html{" > .$ligne["nom"]. </a>"</th>"
+		          echo "<th>".$ligne["NomCircuit"]."</th>";
+		          echo "<th>".$ligne["VilleCircuit"]."</th>";
+		          echo "<th>".$ligne["PaysCircuit"]."</th>";
+		          echo "</tr>";
+		          }
+                $rep ->closeCursor();
+                ?>
+            </tr>
+            
+        </table>
+    </body>
+    
+</html>
