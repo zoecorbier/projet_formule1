a19eba84b59c14e556cd0e6b6727e360840c3bb0
Sophie168-mel <60349664+Sophie168-mel@users.noreply.github.com>
2020-03-17 18:15:29 +0100

    Update enregistrement1.php

--- enregistrement1.php
+++ enregistrement1.php
@@ -7,22 +7,35 @@
 
  function enregistrer($MailUtilisateur,$NomUtilisateur, $PrenomUtilisateur, $AdresseUtilisateur, $CodePostalUtilisateur, $VilleUtilisateur, $PaysUtilisateur, $DateNaissanceUtilisateur, $TelephoneUtilisateur, $mdp){
 
+    $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root','root');
 
-/*
-    $req="insert into Utilisateur (MailUtilisateur,NomUtilisateur,PrenomUtilisateur,AdresseUtilisateur, CodePostalUtilisateur,VilleUtilisateur, PaysUtilisateur,DateNaissanceUtilisateur,TéléphoneUtilisateur,mdp) VALUES ('".$MailUtilisateur."','".$NomUtilisateur."','".$PrenomUtilisateur."','".$AdresseUtilisateur."',".$CodePostalUtilisateur.",'".$VilleUtilisateur."','".$PaysUtilisateur."','".$DateNaissanceUtilisateur."',".$TelephoneUtilisateur.",'".$mdp."')"; */
-    
-    $req="INSERT INTO `Utilisateur` (`MailUtilisateur`, `NomUtilisateur`, `PrenomUtilisateur`, `AdresseUtilisateur`, `CodePostalUtilisateur`, `VilleUtilisateur`, `PaysUtilisateur`, `DateNaissanceUtilisateur`, `TelephoneUtilisateur`, `mdp`) VALUES ('".$MailUtilisateur."', '".$NomUtilisateur."', '".$PrenomUtilisateur."', '".$AdresseUtilisateur."',".$CodePostalUtilisateur.",'".$VilleUtilisateur."','".$PaysUtilisateur."','".$DateNaissanceUtilisateur."',".$TelephoneUtilisateur.",'".$mdp."')";
+    if($bdd->query("SELECT * FROM Utilisateur WHERE
+        MailUtilisateur='$MailUtilisateur'")->fetch()){
+
+        $err='mail';
+
+        echo "<meta http-equiv='refresh' content='2; 
+    URL=new.php?mail=$MailUtilisateur&n=$NomUtilisateur&
+    p=$PrenomUtilisateur&adr=$AdresseUtilisateur
+    &cp=$CodePostalUtilisateur&ville=
+    $VilleUtilisateur&anniversaire=$DateNaissanceUtilisateur
+    &pays=$PaysUtilisateur&num=$TelephoneUtilisateur&erreur=$err'>";
+
+    }
+
+    else{
+   
+    $req="INSERT INTO `Utilisateur` (`MailUtilisateur`, `NomUtilisateur`, `PrenomUtilisateur`, `AdresseUtilisateur`, `CodePostalUtilisateur`, `VilleUtilisateur`, `PaysUtilisateur`, `DateNaissanceUtilisateur`, `TelephoneUtilicasateur`, `mdp`) 
+    VALUES ('".$MailUtilisateur."', '".$NomUtilisateur."', '".$PrenomUtilisateur."', '".$AdresseUtilisateur."',".$CodePostalUtilisateur.",'".$VilleUtilisateur."','".$PaysUtilisateur."','".$DateNaissanceUtilisateur."',".$TelephoneUtilisateur.",'".$mdp."')";
 
     $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root','root');
- $bdd-> query ($req);
+    $bdd-> query ($req);
 
- echo "<meta http-equiv='refresh' content='2; URL=pageacceuil.php'>";
-
-$req ->closeCursor();
-
+    echo "<meta http-equiv='refresh' content='2; URL=accueil.php'>";
+    
+    $req ->closeCursor();
+    }
 }
-
-
 
     $MailUtilisateur=$_GET['mail'];
     $NomUtilisateur=$_GET['n'];
@@ -35,22 +48,33 @@ $req ->closeCursor();
     $TelephoneUtilisateur=$_GET['num'];
     $mdp=$_GET['mdp1'];
 
-   
 
 
- if($_GET['mail']!="" && $_GET['n']!="" && $_GET['p']!="" && $_GET['adr']!="" && $_GET['cp']!="" && $_GET['ville']!="" && $_GET['anniversaire']!=""&& $_GET['pays']!="" && $_GET['num']!=""&& $_GET['mdp1']!="" && $_GET['mdp2']!="" && $_GET['mdp2']==$_GET['mdp1']) {
-   
+    
+
+ if($_GET['mail']!="" && $_GET['n']!="" && $_GET['p']!="" && $_GET['adr']!="" && $_GET['cp']!="" && $_GET['ville']!="" && $_GET['anniversaire']!=""&& 
+    $_GET['pays']!="" && $_GET['num']!=""&& $_GET['mdp1']!="" && $_GET['mdp2']!="" && $_GET['mdp2']==$_GET['mdp1']) 
+  {   
+    
 
      enregistrer($MailUtilisateur,$NomUtilisateur, $PrenomUtilisateur, $AdresseUtilisateur, $CodePostalUtilisateur, $VilleUtilisateur, $PaysUtilisateur, $DateNaissanceUtilisateur, $TelephoneUtilisateur, $mdp);
-
-
-}
+    }
 
 else{
 
+    // erreur vaut $err='mdp'
 
-    echo "<meta http-equiv='refresh' content='2; URL=new.php?mail=$MailUtilisateur&n=$NomUtilisateur&p=$PrenomUtilisateur&adr=$AdresseUtilisateur&cp=$CodePostalUtilisateur&ville=$VilleUtilisateur&anniversaire=$DateNaissanceUtilisateur&pays=$PaysUtilisateur&num=$TelephoneUtilisateur'>";
+    $err='mdp';
+
+    echo "<meta http-equiv='refresh' content='2; 
+    URL=new.php?mail=$MailUtilisateur&
+    n=$NomUtilisateur&p=$PrenomUtilisateur
+    &adr=$AdresseUtilisateur&cp=$CodePostalUtilisateur
+    &ville=$VilleUtilisateur&anniversaire=
+    $DateNaissanceUtilisateur&pays=$PaysUtilisateur
+    &num=$TelephoneUtilisateur&erreur=$err'>";
 }
+
 
 ?>
 
