59eaeb915a890761d47a4963fbad6439ef97584a
Sophie168-mel <60349664+Sophie168-mel@users.noreply.github.com>
2020-03-17 18:13:11 +0100

    Add files via upload

---
+++ connecter1.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+
+$mail=$_POST['mail'];
+$mdp=$_POST['mdp'];
+
+
+$bdd = new 
+PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root','root');
+
+$informations = $bdd-> query ("select * from Utilisateur 
+	where MailUtilisateur='$mail' and mdp='$mdp' ");
+
+
+$informations=$informations->fetch();
+
+
+
+if(!$informations){
+	 echo "<meta http-equiv='refresh' content='2; URL=connexion1.php'>";
+}
+
+else{
+
+	$_SESSION['utilisateur']=array('MailUtilisateur'=>$informations['MailUtilisateur'],
+		'NomUtilisateur'=>$informations['NomUtilisateur'],
+		'PreomUtilisateur'=>$informations['PreomUtilisateur'],
+		'AdresseUtilisateur'=>$informations['AdresseUtilisateur'],
+		'CodePostalUtilisateur'=>$informations['CodePostalUtilisateur'],
+		'PaysUtilisateur'=>$informations['PaysUtilisateur'],
+		'DateNaissanceUtilisateur'=>$informations['DateNaissanceUtilisateur'],
+		'TelephoneUtilisateur'=>$informations['TelephoneUtilisateur'],
+		'mdp'=>$informations['mdp']);
+
+
+	echo "<meta http-equiv='refresh' content='2; URL=accueil.php'>";
+
+}
+
+?>
