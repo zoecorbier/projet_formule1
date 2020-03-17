fecc2d7d7921d7f504bcd7aabf1a5a859faacfef
Sophie168-mel <60349664+Sophie168-mel@users.noreply.github.com>
2020-03-17 18:13:28 +0100

    Add files via upload

---
+++ connexion1.php
@@ -0,0 +1,16 @@
+<a href="new.php"> Inscription </a> 
+
+<?php?>
+
+
+<form method="POST" action="connecter1.php" autocomplete="off"> 
+	</p> 
+	Adresse e-mail 
+	<INPUT required type="text" name="mail" >
+	</p>
+	<p>
+	Mot de passe <INPUT type="password" name="mdp" >
+	</p>
+<br>
+<input type="submit" value="connexion" name="connexion">
+</form>
