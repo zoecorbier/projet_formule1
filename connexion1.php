4897819f053cb70d422c0a061d640783ceda1602
zoe.corbier <47537047+zoecorbier@users.noreply.github.com>
2020-03-30 12:38:12 +0200

    Update connexion1.php

--- connexion1.php
+++ connexion1.php
@@ -1,16 +1,17 @@
-<a href="new.php"> Inscription </a> 
-
-<?php?>
-
-
-<form method="POST" action="connecter1.php" autocomplete="off"> 
-	</p> 
-	Adresse e-mail 
-	<INPUT required type="text" name="mail" >
-	</p>
-	<p>
-	Mot de passe <INPUT type="password" name="mdp" >
-	</p>
-<br>
-<input type="submit" value="connexion" name="connexion">
-</form>
+<!DOCTYPE html>
+<html>
+    <body>
+        <a href="new.php"> Inscription </a> 
+        <form method="POST" action="connecter1.php" autocomplete="off"> 
+	   <p>
+           Adresse e-mail <INPUT required type="text" name="mail" />
+       </p>
+       <br>
+	    <p>
+            Mot de passe <INPUT type="password" name="mdp" />
+	   </p>
+       <br>        
+        <input type="submit" value="connexion" name="connexion"/>
+        </form>
+    </body>
+</html>
