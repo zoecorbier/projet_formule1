c74822e34b75d5584aa54673da9dfba4bf21609c
zoe.corbier <47537047+zoecorbier@users.noreply.github.com>
2020-03-30 12:08:13 +0200

    Update new.php

--- new.php
+++ new.php
@@ -1,54 +1,58 @@
-<! doctype html>
+<! DOCTYPE html>
 <html>
-<head>
-	<meta charset ="UTF-8"/>
-	<title> Inscription </title> 
-</head>
+    <head>
+	   <meta charset ="UTF-8"/>
+	   <title> Inscription </title> 
+    </head>
 
-<body>
+    <body>
 	<h1> Inscription </h1>
-<form method="get" action="enregistrement1.php" autocomplete="off"> 
-<p>
-	Nom de famille <INPUT required type="text" name="n" value=<?php echo $_GET['n'];?>>
-</p>
-<p>
-	Prenom <INPUT required type="text" name="p" value=<?php echo $_GET['p'];?>>
-</p>
-<p>
-	Date de naissance (AAAA-MM-JJ)<INPUT required type="date" name="anniversaire" value=<?php echo $_GET['anniversaire'];?>>
-</p>
-<p>
-	Adresse <INPUT type="text" name="adr" value=<?php echo $_GET['adr'];?>>
-</p>
-<p>
-	Code postal <INPUT type="number" name="cp" value=<?php echo $_GET['cp'];?>>
-</p>
-<p>
-	Ville <INPUT type="text" name="ville" value=<?php echo $_GET['ville'];?>>
-</p>
-<p>
-	Pays <INPUT type="text" name="pays" value=<?php echo $_GET['pays'];?>>
-</p>
-<p>
-	Numéro de téléphone <INPUT required type="number" name="num" value=<?php echo $_GET['num'];?>>
-</p>
-<p>
-	Adresse électronique <INPUT required type="email" name="mail" value=<?php echo $_GET['mail'];?>>
-	<?php if($_GET['erreur']=='mail') echo "<br><span style='color : red'> ⛔ Ce mail existe déjà , utilisez un autre </span>";?>
+    <form method="get" action="enregistrement1.php" autocomplete="off"> 
+        <p>
+	       Nom de famille <INPUT required type="text" name="n" value=<?php echo $_GET['n'];?>/>
+        </p>
+        <p>
+	       Prenom <INPUT required type="text" name="p" value=<?php echo $_GET['p'];?>/>
+        </p>
+        <p>
+	       Date de naissance (AAAA-MM-JJ)<INPUT required type="date" name="anniversaire" value=<?php echo $_GET['anniversaire'];?>/>
+        </p>
+        <p>
+	       Adresse <INPUT type="text" name="adr" value=<?php echo $_GET['adr'];?>/>
+        </p>
+        <p>
+	       Code postal <INPUT type="number" name="cp" value=<?php echo $_GET['cp'];?>/>
+        </p>
+        <p>
+	       Ville <INPUT type="text" name="ville" value=<?php echo $_GET['ville'];?>/>
+        </p>
+        <p>
+	       Pays <INPUT type="text" name="pays" value=<?php echo $_GET['pays'];?>/>
+        </p>
+        <p>
+	       Numéro de téléphone <INPUT required type="number" name="num" value=<?php echo $_GET['num'];?>/>
+        </p>
+        <p>
+	       Adresse électronique <INPUT required type="email" name="mail" value=<?php echo $_GET['mail'];?>
+	       <?php 
+                if($_GET['erreur']=='mail'){
+                    echo "<br><span style='color : red'> ⛔ Ce mail existe déjà , utilisez un autre </span>";}
+            ?>/>
 
-</p>
-<p>
-	Mot de passe <INPUT required type="password" name="mdp1" value="">
-</p>
-<p>
-	Confirmer votre mot de passe <INPUT required type="password" name="mdp2" value="">
-	<?php if($_GET['erreur']=='mdp') echo "<br><span style='color : red'> ⛔ Les 2 mots de passe ne correspondent pas</span>";?>
-
-</p>
- <p>
- 	 <INPUT type="submit" value="S'inscrire" name="sinscrire">
- </p>
-</form>
-
-</body>
+        </p>
+        <p>
+	           Mot de passe <INPUT required type="password" name="mdp1" value=""/>
+        </p>
+        <p>
+	           Confirmer votre mot de passe <INPUT required type="password" name="mdp2" value=""/>
+	           <?php 
+                    if($_GET['erreur']=='mdp') {
+                        echo "<br><span style='color : red'> ⛔ Les 2 mots de passe ne correspondent pas</span>";}
+                ?>
+        </p>
+        <p>
+ 	          <INPUT type="submit" value="S'inscrire" name="sinscrire"/>
+        </p>
+    </form>
+    </body>
 </html>
