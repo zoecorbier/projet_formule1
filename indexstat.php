d819820e0c960c19c98b18e010b613c9dec805a4
zoe.corbier <47537047+zoecorbier@users.noreply.github.com>
2020-04-06 10:22:59 +0200

    Update indexstat.php

--- indexstat.php
+++ indexstat.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="../style/pages.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="../style/pagesdeux.css" type="text/css" media="screen" />
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     </head>
     
