351a9dd9ddb9112cdf209a4105e736d66f48a1c5
Sophie168-mel <60349664+Sophie168-mel@users.noreply.github.com>
2020-04-10 12:14:07 +0200

    Update connexion1.php

--- connexion1.php
+++ connexion1.php
@@ -1,17 +1,26 @@
+
 <!DOCTYPE html>
+
 <html>
-    <body>
-        <a href="new.php"> Inscription </a> 
-        <form method="POST" action="connecter1.php" autocomplete="off"> 
-	   <p>
-           Adresse e-mail <INPUT required type="text" name="mail" />
-       </p>
-       <br>
-	    <p>
-            Mot de passe <INPUT type="password" name="mdp" />
-	   </p>
-       <br>        
-        <input type="submit" value="connexion" name="connexion"/>
-        </form>
-    </body>
+    <head>
+        <link rel="stylesheet" href="page.css" type="text/css" media="screen" />
+     </head>
+
+     <body>
+<a href="new.php"> Inscription </a> 
+
+<?php?>
+<form method="POST" action="connecter1.php" autocomplete="off" > 
+	</p> 
+	Adresse e-mail <INPUT required type="text" name="mail" >
+	</p>
+	<p>
+	Mot de passe <INPUT type="password" name="mdp">
+	</p>
+<br>
+<input type="submit" value="connexion" name="connexion">
+</form>
+
+</body>
+
 </html>
