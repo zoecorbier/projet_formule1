c324160754d19a8f4a5c4f541bc6dc0b37cc5bb0
Sophie168-mel <60349664+Sophie168-mel@users.noreply.github.com>
2020-04-10 12:33:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,33 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="style/pagesdeux.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="page.css" type="text/css" media="screen" />
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     </head>
     <body>
         <div class="projet">
-            <img class="imgpres" src="images/f1deux.jpg">
+            <img height='100' width='100' class="imgpres" src="image.png">
             <p id="titre">Formule 1</p>
-            
+         
+         <?php if(!isset($_SESSION['utilisateur'])){ ?>   
             <ul>
-                <li id="co"><a href="identifification/connexion1.php">Se connecter</a></li>
-                <li><a href="identifification/enregistrement1.php">Créer un compte</a></li>
+                <li id="co"><a href="../projet/connexion1.php">Se connecter</a></li>
+                <li><a href="../projet/new.php">Créer un compte</a></li>
             </ul>
+
+            <?php } else {
+
+
+
+    echo "<ul>
+            <li><font color='blue'>Bonjour "." ".$_SESSION['utilisateur']['PrenomUtilisateur']." ".$_SESSION['utilisateur']['NomUtilisateur']."</font></li>
+            <li><a href='quizz.php'>Participer au quizz</a></li>
+            <li><a href='deconnexion1.php'> Se déconnecter </a></li>
+        </ul>
+    ";
+  }
+    ?>
+
         </div>
         
         <p id="presentation">Bienvenue sur notre site, ici vous aurez librement accès à des renseignements sur la formule 1 et en créant un compte vous aurez la possibilité de participer à un quizz sur ce même thème</p>
@@ -34,7 +49,7 @@
 		      </tr>
             
                 <?php
-                 $bdd = new PDO('mysql:host=localhost;dbname=Projet_Formule1;charset=utf8', 'root', 'root');
+                 $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'root');
                 $rep = $bdd->query('select * from Pilote LIMIT 10');
                 
                 $ligne = $rep->fetch();
@@ -47,13 +62,12 @@
 		          echo "<td>".$ligne["NationalitéPilote"]."</td>";
                   echo "</tr>";
 		          }
-		
                 $rep ->closeCursor();
                 ?>
             
         </table>
         
-        <a class="bt" href="stat/indexstat.php"> Plus de données </a>
+        <a class="bt" href="indexstat.php"> Plus de données </a>
 
  	</body>
  </html> 
