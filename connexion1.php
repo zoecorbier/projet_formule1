9b8e6fac657236bdf8d1e16b987480a95fff8a36
manon999 <60349878+manon999@users.noreply.github.com>
2020-04-14 18:01:35 +0200

    Update connexion1.php

--- connexion1.php
+++ connexion1.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-
 <html>
-    <head>
-	<link rel="stylesheet" href="../style/pages.css" type="text/css" media="screen"/>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-     </head>
 
-     <body>
-         
-    
-        <div id="menu" class="projet">
+<head>
+	<title>Connexion</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<link rel="stylesheet" href="../projet/pagedeux.css" type="text/css" media="screen" />
+</head>
+
+<body>
+	<div id="menu" class="projet">
             <img class="imgpres" src="../images/f1.jpg">
             <p id="titre">Formule 1</p>
             
@@ -17,17 +16,25 @@
                 <li id="co"><a href="../identifification/enregistrement1.php">Créer un compte</a></li>
                 <li><a href="../index.php">Acceuil</a></li>
             </ul>
-        </div>
+        </div><br>
 
-        
-         <form method="POST" action="connecter1.php" autocomplete="off" > 
-             <INPUT required type="text" name="mail" placeholder="Adresse e-mail"/>
-             <br>
-             <INPUT type="password" name="mdp" placeholder="Mot de passe"/>
-             <br>
-             <input class="bt" type="submit" value="Connexion" name="connexion">
-         </form>
-       
+	</br><br>
+	<center><h1 class="connexion">Connexion</h1></center>
+	<center>
+	
+	<form method="POST" action="connecter1.php" autocomplete="off" >
+	
+	<table>
+	
+ 		<br><tr><td>E-mail : </td><td> <input type="text" name="mail" value="" /></td></tr>
+ 		<tr><td>Mot de passe : </td><td> <input type="password" name="mdp" /></td></tr>
+ 		<tr><td><input type="submit" value="se connecter"></td><td></td></tr>
+	
+	</table>
+
+	</form></center>
+	
+	<p>Si vous n'avez pas de compte, veuillez en créer un : <a href="inscription.php" >Creer un compte</a></p>
+
 </body>
-
-</html>
+</html> 
