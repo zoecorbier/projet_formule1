d14a3281c3c29628c9bbe46d80bb11d4a9dfbba7
zoe.corbier <47537047+zoecorbier@users.noreply.github.com>
2020-04-14 16:35:15 +0200

    Update connexion1.php

--- connexion1.php
+++ connexion1.php
@@ -1,24 +1,33 @@
-
 <!DOCTYPE html>
 
 <html>
     <head>
-        <link rel="stylesheet" href="page.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="../style/pages.css" type="text/css" media="screen"/>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
      </head>
 
      <body>
-<a href="new.php"> Inscription </a> 
+         
+    
+        <div id="menu" class="projet">
+            <img class="imgpres" src="../images/f1.jpg">
+            <p id="titre">Formule 1</p>
+            
+            <ul>
+                <li id="co"><a href="../identifification/enregistrement1.php">Créer un compte</a></li>
+                <li><a href="../index.php">Acceuil</a></li>
+            </ul>
+        </div>
 
-<?php?>
-<form method="POST" action="connecter1.php" autocomplete="off" > 
-
-	Adresse e-mail <INPUT required type="text" name="mail" />
-	<br>
-	Mot de passe <INPUT type="password" name="mdp"/>
-	<br>
-	<input type="submit" value="connexion" name="connexion">
-</form>
-
+        
+         <form method="POST" action="connecter1.php" autocomplete="off" > 
+             <INPUT required type="text" name="mail" placeholder="Adresse e-mail"/>
+             <br>
+             <INPUT type="password" name="mdp" placeholder="Mot de passe"/>
+             <br>
+             <input class="bt" type="submit" value="Connexion" name="connexion">
+         </form>
+       
 </body>
 
 </html>
